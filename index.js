012e74fd299bb494be390cb4d79c9c38f088c098
Andrex Lorenzini <andrea.lorenzini4@gmail.com>
2021-04-13 19:03:37 +0200

    Chpt 8 - Test Driven Devel before refactoring

--- index.js
+++ index.js
@@ -1,62 +1,34 @@
-// Reverses a string. Works with emopjis.
-// Example sentence to try it out: The quick brown 🦊 and the faithful 🐕
-function reverse(string) {
-  return Array.from(string).reverse().join("");
-}
+module.exports = Phrase;
 
-// Adds the .reverse() method to all strings (to 'String.prototype'):
-// String.prototype.reverse = function() {
-//   return Array.from(this).reverse().join("");
-// }
-// // Exercise: this method should return true if a string is empty or consists
-// // only of whitespace. (The exercise said to add it to String.prototype.)
-// // In the RegEx, '^' means 'Start of string', '$' means 'End of string'.
-// String.prototype.isEmpty = function() {
-//   if ( this.length === 0 || !!this.match(/^\s+$/) ) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// // Exercise: add a sliceLast method to Array.prototype that returns the last
-// // element in an array:
-// Array.prototype.sliceLast = function() {
-//   return this.slice(-1)[0];
-// }
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
 
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
-  // Returns content of a string in lowercase.
-  this.processor = function(string) {
-    this.string = string;
-    return this.string.toLowerCase();
-  }
+
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
   }
-  // Returns true for a palindrome, false otherwise. Case-insensitive.
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    let theLetters = [];
+    for (let i = 0; i < this.content.length; i++) {
+      if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+        theLetters.push(this.content.charAt(i));
+      }
+    }
+    return theLetters.join("");
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
-    // If you add the .reverse() method permanenntly to String.prototype (see
-    // above), it becomes like this:
-    // return this.processedContent() === this.processedContent().reverse();
-  }
-  // Returns content in all caps.
-  this.louder = function() {
-    return this.content.toUpperCase();
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-  // Overrides the processedContent method in its prototype object ('Phrase'),
-  // so that 'translation' will be tested as a palindrome instead of 'content':
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Phrase object (with palindrome detector)
+
+This is a sample NPM module created in [*Learn Enough JavaScript to Be Dangerous*](https://www.learnenough.com/javascript-tutorial) by Michael Hartl.
+
+The module can be used as follows:
+
+```
+$ npm install --global taliesin12345-palindrome
+$ vim test.js
+let Phrase = require("taliesin12345-palindrome");
+let napoleonsLament = new Phrase("Able was I, ere I saw Elba.");
+console.log(napoleonsLament.palindrome());
+$ node test.js
+true
+```
