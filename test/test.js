458700113da8ae7b3c3609f4a8603a3c4c57116e
Andrex Lorenzini <andrea.lorenzini4@gmail.com>
2021-04-15 13:13:32 +0200

    Fixed bug on evalutaing an empty string

--- test/test.js
+++ test/test.js
@@ -25,6 +25,11 @@ describe("Phrase", function() {
       assert(punctuatedPalindrome.palindrome());
     });
 
+    it("should return false for an empty string", function() {
+      let emptyPhrase = new Phrase("");
+      assert(!emptyPhrase.palindrome());
+    });
+
   });
 
   describe("#letters", function() {
